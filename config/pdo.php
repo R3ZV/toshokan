211e263362bc408f007ffad31934a31d613264fd
R3ZV <razvan.maracine@protonmail.com>
2024-11-27 20:32:36 +0200

    PDO

--- config/pdo.php
+++ config/pdo.php
@@ -8,11 +8,21 @@ $DB_NAME = $ENV["DB_NAME"];
 $DB_PASSWORD = $ENV["DB_PASSWORD"];
 $DB_USER = $ENV["DB_USER"];
 
-$pdo = new PDO(
-   'mysql:host=' . $DB_HOST . ';port=3306;dbname=' . $DB_NAME,
-   $DB_USER,
-   $DB_PASSWORD
-);
+$pdo = null
+try {
+    $pdo = new PDO(
+       'mysql:host=' . $DB_HOST . ';port=3306;dbname=' . $DB_NAME,
+       $DB_USER,
+       $DB_PASSWORD
+    );
+}
+catch(PDOException $err) {
+    echo $err->getMessage() . "<br>";
+
+    echo 'mysql:host=' . $DB_HOST . ';port=3306;dbname=' . $DB_NAME . "<br>";
+    echo $DB_USER . "<br>";
+    echo $DB_PASSWORD . "<br>";
+}
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
