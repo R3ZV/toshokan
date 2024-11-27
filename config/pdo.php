1ab2f51a0a9d57feff0f7d57d5457f9e199ee6a5
R3ZV <razvan.maracine@protonmail.com>
2024-11-27 20:45:48 +0200

    Know I know

--- config/pdo.php
+++ config/pdo.php
@@ -8,7 +8,7 @@ $DB_NAME = $ENV["DB_NAME"];
 $DB_PASSWORD = $ENV["DB_PASSWORD"];
 $DB_USER = $ENV["DB_USER"];
 
-$pdo = null
+$pdo = null;
 try {
     $pdo = new PDO(
        'mysql:host=' . $DB_HOST . ';port=3306;dbname=' . $DB_NAME,
