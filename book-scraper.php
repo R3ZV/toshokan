8bb039ff0dafcd4c4a8ecc07726ab01c83fcd2e6
R3ZV <razvan.maracine@protonmail.com>
2025-01-09 15:56:49 +0200

    Scrape

--- book-scraper.php
+++ book-scraper.php
@@ -14,7 +14,22 @@ $spanClass = "bookauthor";
 $authors_qry = "//span[contains(concat(' ', normalize-space(@class), ' '), ' $spanClass ')]";
 $authors = $xpath->query($authors_qry);
 
-foreach ($authors as $author) {
-    echo "Author: " . $author->nodeValue . "\n";
+$h3Class = "booktitle";
+$titles_qry = "//h3[contains(concat(' ', normalize-space(@class), ' '), ' $h3Class ')]";
+$titles = $xpath->query($titles_qry);
+
+$span2Class = "resultDetails";
+$year_qry = "//span[contains(concat(' ', normalize-space(@class), ' '), ' $span2Class ')]";
+$years = $xpath->query($year_qry);
+
+for ($i = 0; $i < count($years); $i++) {
+    $sql = sprintf('INSERT INTO books (title, author, description, genre, stock, published)
+        VALUES ("%s", "%s", "No description.", "Science Fiction", 0, %s);',
+        trim($titles[$i]->nodeValue),
+        substr(trim($authors[$i]->nodeValue), 3),
+        substr(trim($years[$i]->nodeValue), 19, 4)
+    );
+
+    echo $sql . "\n";
 }
 ?>
